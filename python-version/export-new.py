80feb9360d479e8c60040e93297b25c8aec880de
doug-hahn <dhahn@tamu.edu>
2021-11-17 10:35:46 -0600

    modify the suppression flag being set
    Account for X holding and only 1 is suppressed

--- python-version/export-new.py
+++ python-version/export-new.py
@@ -15,13 +15,21 @@ print("...starting export");
 timestr = time.strftime("%Y%m%d-%H%M%S")
 logging.basicConfig(filename=timestr + "-export.log")
 
-#LOCAL DB
+#PRODUCTION DB
 DATABASE_HOST = "redacted"
 DATABASE_USERNAME = "redacted"
 DATABASE_PASSWORD = "redacted"
 DATABASE_PORT = 5432
 DATABASE_NAME = "redacted"
 TENANT = "redacted"
+
+#PRE PRODUCTION DB
+#DATABASE_HOST = "redacted"
+#DATABASE_USERNAME = "redacted"
+#DATABASE_PASSWORD = "redacted"
+#DATABASE_PORT = 5432
+#DATABASE_NAME = "redacted"
+#TENANT = "redacted"
 
 count = 0
 folio_db = psycopg2.connect(
@@ -46,7 +54,8 @@ for m in materialTypes:
 locLookup = {}
 locCursor = folio_db.cursor(cursor_factory=psycopg2.extras.DictCursor)
 select_all_loc = '''
-select id, jsonb->>'discoveryDisplayName' as name from {}_mod_inventory_storage.location'''.format(TENANT)
+select id, jsonb->>'name' as name from {}_mod_inventory_storage.location'''.format(TENANT)
+
 locCursor.execute(select_all_loc)
 locations = locCursor.fetchall()
 for l in locations:
@@ -67,12 +76,11 @@ cursor = folio_db.cursor(name='folio',cursor_factory=psycopg2.extras.DictCursor)
 cursor.itersize=300000
 #from {}_mod_marc_storage.marc_record'''.format(TENANT)
 
-
 #select_ids_sql = '''
 #select
 #id, 
 #instance_id 
-#from {}_mod_source_record_storage.records_lb where state = {} and (suppress_discovery = False  or suppress_discovery is null)'''.format(TENANT,"'ACTUAL'")
+#from {}_mod_source_record_storage.records_lb where state = {} and (suppress_discovery = False or suppress_discovery is null)'''.format(TENANT,"'ACTUAL'")
 
 select_ids_sql = '''
 select
@@ -81,10 +89,15 @@ suppress_discovery,
 instance_id 
 from {}_mod_source_record_storage.records_lb where state = {}'''.format(TENANT,"'ACTUAL'")
 
-#print("executing query")
-cursor.execute(select_ids_sql)
+#select_ids_sql = '''
+#select
+#id,
+#suppress_discovery, 
+#instance_id 
+#from {}_mod_source_record_storage.records_lb where instance_id = '5101ae83-6a0b-3ede-a170-52a8705a805f' AND state = {}'''.format(TENANT,"'ACTUAL'")
 
-discoverySuppress = "0"
+print("executing query")
+cursor.execute(select_ids_sql)
 
 while True:
 	print("in the while true - fetching...")
@@ -114,7 +127,11 @@ while True:
 				marcJsonAsString = json.dumps(marcRow['marc'])
 				marcString = marcJsonAsString.encode('utf-8').strip()
 				#print(marcJsonAsString);
+                
 				for record in JSONReader(marcJsonAsString):
+					discoverySuppressItem = "0"
+					discoverySuppressHold = "0"
+					discoverySuppress = "1"                
 					#write MARC JSON to output file
 					#ADD A 998 FOR EACH HOLDING RECORD
 					if record['6xx'] is not None:
@@ -143,66 +160,93 @@ while True:
 #					print(select_holding_sql)
 					marcRecordCursor.execute(select_holding_sql)
 					holdingRows = marcRecordCursor.fetchall()
+                    
 					for holding in holdingRows:
 					
-						#print(holding[6])            
+#						print("Begin Holding Record:  Holding Suppressed 1=YES, 0=NO: " + holding[6])            
                     
-						discoverySuppress = "0"
+						discoverySuppressHold = "0"
                         
 						if not (holding[6] is None):
-							tmp_hold = holding[6]
-							if (tmp_hold == "true"):
-								discoverySuppress = "1"
-                        
-						holdingsStatements = holding['holdingstatements']
-						rowHoldingsId = holding['id']
-						newField = Field(tag = '998',
-								  indicators = [' ',' '],
-								  subfields = ['a',holding.get('callnumber',''),
-											'l',locLookup.get(holding.get('permanentlocationid',''),'')])
-						for statement in holdingsStatements:
-							if statement is not None:
-								newField.add_subfield('s',statement.get('statement','').replace('Extent of ownership:',''));
-						record.add_field(newField)
-						#ADD AN 952 FOR EACH ITEM
-						select_item_sql = '''
-						select id, materialtypeid, 
-						jsonb->>'effectiveLocationId' as effectivelocationid, 
-						jsonb->>'barcode' as barcode, 
-						jsonb->'effectiveCallNumberComponents'->>'prefix' as prefix,
-						jsonb->'effectiveCallNumberComponents'->>'typeId' as callnotype,
-						jsonb->'effectiveCallNumberComponents'->>'callNumber' as callnumber,
-						jsonb->'effectiveCallNumberComponents'->>'discoverySuppress' as discoverySuppress
-						from {}_mod_inventory_storage.item where 
-						holdingsrecordid = '{}' '''.format(TENANT,rowHoldingsId)
-#						print(select_item_sql)
-						marcRecordCursor.execute(select_item_sql)
-						itemRows = marcRecordCursor.fetchall()
-						for item in itemRows:
-                        
-							if not (item[7] is None):
-								tmp_hold = item[7]
-								if (tmp_hold == "true"):
-									discoverySuppress = "1"                                
-                        
-							callNoToUse = item.get('callnumber','na')
-							#print(callNoToUse)
-							prefix = item.get('prefix',None)
-							if (prefix is not None):
-								callNoToUse = prefix + " " + callNoToUse
-							record.add_field(
-								Field(tag = '952',
-									indicators = [' ',' '],
-									subfields = ['m',item.get('barcode',''),
-									'j',callNoTypeLookup.get(item.get('callnotype',''),''),
-									'd',locLookup.get(item.get('effectivelocationid'),''),
-									'i',materialTypeLookup.get(item.get('materialtypeid'),''),
-									'e',callNoToUse]))
-							if (len(record.leader) < 24):
-								logging.error("BAD LEADER" + record.leader + " " + str(row))
-								record.leader = "{:<24}".format(record.leader)
+							tmp_hold_supress = holding[6]
+							if (tmp_hold_supress == "true"):
+#								print("I should be supressed Set Hold DS = 1")
+								discoverySuppressHold = "1"
 
-					#ADD AN 999t FOR EACH ITEM to show it is not suppressed
+
+						if (discoverySuppressHold != "1"):
+							discoverySuppress = "0"
+							holdingsStatements = holding['holdingstatements']
+							rowHoldingsId = holding['id']
+							newField = Field(tag = '998',
+									  indicators = [' ',' '],
+									  subfields = ['a',holding.get('callnumber',''),
+												'l',locLookup.get(holding.get('permanentlocationid',''),'')])
+                                            
+							for statement in holdingsStatements:
+								if statement is not None:
+									newField.add_subfield('s',statement.get('statement','').replace('Extent of ownership:',''));
+                                
+                                
+							record.add_field(newField)
+							#ADD AN 952 FOR EACH ITEM
+							select_item_sql = '''
+							select id, materialtypeid, 
+							jsonb->>'effectiveLocationId' as effectivelocationid, 
+							jsonb->>'barcode' as barcode, 
+							jsonb->'effectiveCallNumberComponents'->>'prefix' as prefix,
+							jsonb->'effectiveCallNumberComponents'->>'typeId' as callnotype,
+							jsonb->'effectiveCallNumberComponents'->>'callNumber' as callnumber,
+							jsonb->'effectiveCallNumberComponents'->>'discoverySuppress' as discoverySuppress
+							from {}_mod_inventory_storage.item where 
+							holdingsrecordid = '{}' '''.format(TENANT,rowHoldingsId)
+#							print(select_item_sql)
+							marcRecordCursor.execute(select_item_sql)
+							itemRows = marcRecordCursor.fetchall()
+							for item in itemRows:
+
+								discoverySuppressItem = "0"
+#								if not (item[7] is None):
+#									print("Item Suppressed 1=YES 0=NO:" +item[7]) 
+                            
+								if not (item[7] is None):
+									tmp_item_supress = item[7]
+									if (tmp_item_supress == "true"):
+#										print("I should be supressed Set Item DS = 1")                                
+										discoverySuppressItem = "1"
+                        
+								callNoToUse = item.get('callnumber','na')
+								#print(callNoToUse)
+								prefix = item.get('prefix',None)
+
+									
+                            
+								if (prefix is not None):
+									callNoToUse = prefix + " " + callNoToUse
+
+#								print('discoverySuppressItem: ' + discoverySuppressItem)
+#								print('discoverySuppressHold: ' + discoverySuppressHold)
+#								print('discoverySuppress: ' + discoverySuppress)
+
+#								print('Add 952 Field')
+                            
+								record.add_field(
+									Field(tag = '952',
+										indicators = [' ',' '],
+										subfields = ['m',item.get('barcode',''),
+										'j',callNoTypeLookup.get(item.get('callnotype',''),''),
+										'd',locLookup.get(item.get('effectivelocationid'),''),
+										'i',materialTypeLookup.get(item.get('materialtypeid'),''),
+										'e',callNoToUse]))
+								if (len(record.leader) < 24):
+									logging.error("BAD LEADER" + record.leader + " " + str(row))
+									record.leader = "{:<24}".format(record.leader)
+
+#								print('End Add 952 Field')
+
+#						print("End Holding Record")            
+					
+                    #ADD AN 999t FOR EACH ITEM to show it is not suppressed
 					record['999'].add_subfield('t',str(discoverySuppress))
 					# DCH 
 
@@ -218,7 +262,6 @@ while True:
 					logging.error(e)
 					continue
 		writer.close()
-		break
 	else:
 		print("in the else --> finishing")
 		break
